d8156a025d0f6746229426fe494d3aefd61dcb3c
0jikuji0 <the.jikuji_dev@proton.me>
2026-04-19 18:39:51 +0200

    feat: setup database connection and migration system

--- internal/mariadb_driver.go
+++ internal/mariadb_driver.go
@@ -1,15 +1,15 @@
 package internal
 
 import (
-	"database/sql"
 	"fmt"
 	"go-upcycle_connect-backend/utils/log"
 	"strconv"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
 )
 
-func NewDatabase(user string, password string, host string, port string, dbname string) *sql.DB {
+func NewDatabase(user string, password string, host string, port string, dbname string) *sqlx.DB {
 	var intPort, errorToConvert = strconv.Atoi(port)
 
 	if errorToConvert != nil {
@@ -18,7 +18,7 @@ func NewDatabase(user string, password string, host string, port string, dbname 
 
 	var sqlInfo = fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", user, password, host, intPort, dbname)
 
-	conn, err := sql.Open("mysql", sqlInfo)
+	conn, err := sqlx.Open("mysql", sqlInfo)
 
 	if err != nil {
 		log.Fatal(err)
