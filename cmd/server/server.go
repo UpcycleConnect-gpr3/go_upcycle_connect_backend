66a299fea382f44136894a3fc02246aadc8b853f
FortyTwo-Dev <91018423+FortyTwo-Dev@users.noreply.github.com>
2026-04-19 22:49:46 +0200

    fix and refactor

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -10,7 +10,6 @@ import (
 	"go-upcycle_connect-backend/app/handlers/step_handlers"
 	"go-upcycle_connect-backend/app/middleware/auth_middleware"
 	"go-upcycle_connect-backend/app/middleware/ratelimit_middleware"
-	"go-upcycle_connect-backend/app/middleware/source_middleware"
 	"go-upcycle_connect-backend/config"
 	"go-upcycle_connect-backend/database"
 	"net/http"
@@ -54,7 +53,7 @@ func Start() {
 	limiterHigh := ratelimit_middleware.NewRateLimiter(60, 1*time.Minute)
 
 	// Container
-	containerBackoffice := source_middleware.Container("go-upcycle_connect-backend")
+	//containerBackoffice := source_middleware.Container("go-upcycle_connect-backend")
 
 	// Health
 	http.HandleFunc("GET /health/{$}", metric_handlers.Health)
@@ -62,61 +61,61 @@ func Start() {
 	// Delivery Method routes
 	http.HandleFunc("GET /delivery-methods", limiterHigh.RateLimit(delivery_method_handlers.GetDeliveryMethodsHandler))
 	http.HandleFunc("GET /delivery-methods/{id}", limiterHigh.RateLimit(delivery_method_handlers.GetDeliveryMethodHandler))
-	http.HandleFunc("POST /delivery-methods", limiterMedium.RateLimit(containerBackoffice(auth_middleware.IsAuth(delivery_method_handlers.CreateDeliveryMethodHandler))))
-	http.HandleFunc("PUT /delivery-methods/{id}", limiterMedium.RateLimit(containerBackoffice(auth_middleware.IsAuth(delivery_method_handlers.UpdateDeliveryMethodHandler))))
-	http.HandleFunc("DELETE /delivery-methods/{id}", limiterMedium.RateLimit(containerBackoffice(auth_middleware.IsAuth(delivery_method_handlers.DeleteDeliveryMethodHandler))))
+	http.HandleFunc("POST /delivery-methods", limiterMedium.RateLimit(auth_middleware.IsAuth(delivery_method_handlers.CreateDeliveryMethodHandler)))
+	http.HandleFunc("PUT /delivery-methods/{id}", limiterMedium.RateLimit(auth_middleware.IsAuth(delivery_method_handlers.UpdateDeliveryMethodHandler)))
+	http.HandleFunc("DELETE /delivery-methods/{id}", limiterMedium.RateLimit(auth_middleware.IsAuth(delivery_method_handlers.DeleteDeliveryMethodHandler)))
 
 	// Event routes
 	http.HandleFunc("GET /events", limiterHigh.RateLimit(event_handlers.GetEventsHandler))
 	http.HandleFunc("GET /events/{id}", limiterHigh.RateLimit(event_handlers.GetEventHandler))
-	http.HandleFunc("POST /events", limiterMedium.RateLimit(containerBackoffice(auth_middleware.IsAuth(event_handlers.CreateEventHandler))))
-	http.HandleFunc("PUT /events/{id}", limiterMedium.RateLimit(containerBackoffice(auth_middleware.IsAuth(event_handlers.UpdateEventHandler))))
-	http.HandleFunc("DELETE /events/{id}", limiterMedium.RateLimit(containerBackoffice(auth_middleware.IsAuth(event_handlers.DeleteEventHandler))))
+	http.HandleFunc("POST /events", limiterMedium.RateLimit(auth_middleware.IsAuth(event_handlers.CreateEventHandler)))
+	http.HandleFunc("PUT /events/{id}", limiterMedium.RateLimit(auth_middleware.IsAuth(event_handlers.UpdateEventHandler)))
+	http.HandleFunc("DELETE /events/{id}", limiterMedium.RateLimit(auth_middleware.IsAuth(event_handlers.DeleteEventHandler)))
 	http.HandleFunc("GET /events/{id}/steps", limiterHigh.RateLimit(event_handlers.GetEventStepsHandler))
-	http.HandleFunc("POST /events/{id}/steps", limiterMedium.RateLimit(containerBackoffice(auth_middleware.IsAuth(event_handlers.CreateEventStepHandler))))
+	http.HandleFunc("POST /events/{id}/steps", limiterMedium.RateLimit(auth_middleware.IsAuth(event_handlers.CreateEventStepHandler)))
 
 	// Event Step routes
 	http.HandleFunc("GET /event-steps", limiterHigh.RateLimit(event_step_handlers.GetEventStepsHandler))
 	http.HandleFunc("GET /event-steps/{id}", limiterHigh.RateLimit(event_step_handlers.GetEventStepHandler))
-	http.HandleFunc("POST /event-steps", limiterMedium.RateLimit(containerBackoffice(auth_middleware.IsAuth(event_step_handlers.CreateEventStepHandler))))
-	http.HandleFunc("PUT /event-steps/{id}", limiterMedium.RateLimit(containerBackoffice(auth_middleware.IsAuth(event_step_handlers.UpdateEventStepHandler))))
-	http.HandleFunc("DELETE /event-steps/{id}", limiterMedium.RateLimit(containerBackoffice(auth_middleware.IsAuth(event_step_handlers.DeleteEventStepHandler))))
+	http.HandleFunc("POST /event-steps", limiterMedium.RateLimit(auth_middleware.IsAuth(event_step_handlers.CreateEventStepHandler)))
+	http.HandleFunc("PUT /event-steps/{id}", limiterMedium.RateLimit(auth_middleware.IsAuth(event_step_handlers.UpdateEventStepHandler)))
+	http.HandleFunc("DELETE /event-steps/{id}", limiterMedium.RateLimit(auth_middleware.IsAuth(event_step_handlers.DeleteEventStepHandler)))
 
 	// Object routes
 	http.HandleFunc("GET /objects", limiterHigh.RateLimit(object_handlers.GetObjectsHandler))
 	http.HandleFunc("GET /objects/{id}", limiterHigh.RateLimit(object_handlers.GetObjectHandler))
-	http.HandleFunc("POST /objects", limiterMedium.RateLimit(containerBackoffice(auth_middleware.IsAuth(object_handlers.CreateObjectHandler))))
-	http.HandleFunc("PUT /objects/{id}", limiterMedium.RateLimit(containerBackoffice(auth_middleware.IsAuth(object_handlers.UpdateObjectHandler))))
-	http.HandleFunc("DELETE /objects/{id}", limiterMedium.RateLimit(containerBackoffice(auth_middleware.IsAuth(object_handlers.DeleteObjectHandler))))
+	http.HandleFunc("POST /objects", limiterMedium.RateLimit(auth_middleware.IsAuth(object_handlers.CreateObjectHandler)))
+	http.HandleFunc("PUT /objects/{id}", limiterMedium.RateLimit(auth_middleware.IsAuth(object_handlers.UpdateObjectHandler)))
+	http.HandleFunc("DELETE /objects/{id}", limiterMedium.RateLimit(auth_middleware.IsAuth(object_handlers.DeleteObjectHandler)))
 	http.HandleFunc("GET /objects/{id}/score", limiterHigh.RateLimit(object_handlers.GetObjectScoreHandler))
 	http.HandleFunc("GET /objects/{id}/delivery-methods", limiterHigh.RateLimit(object_handlers.GetObjectDeliveryMethodsHandler))
-	http.HandleFunc("POST /objects/{id}/delivery-methods/{deliveryMethodId}", limiterMedium.RateLimit(containerBackoffice(auth_middleware.IsAuth(object_handlers.LinkDeliveryMethodHandler))))
-	http.HandleFunc("DELETE /objects/{id}/delivery-methods/{deliveryMethodId}", limiterMedium.RateLimit(containerBackoffice(auth_middleware.IsAuth(object_handlers.UnlinkDeliveryMethodHandler))))
+	http.HandleFunc("POST /objects/{id}/delivery-methods/{deliveryMethodId}", limiterMedium.RateLimit(auth_middleware.IsAuth(object_handlers.LinkDeliveryMethodHandler)))
+	http.HandleFunc("DELETE /objects/{id}/delivery-methods/{deliveryMethodId}", limiterMedium.RateLimit(auth_middleware.IsAuth(object_handlers.UnlinkDeliveryMethodHandler)))
 	http.HandleFunc("GET /objects/{id}/projects", limiterHigh.RateLimit(object_handlers.GetObjectProjectsHandler))
-	http.HandleFunc("POST /objects/{id}/projects/{projectId}", limiterMedium.RateLimit(containerBackoffice(auth_middleware.IsAuth(object_handlers.LinkProjectHandler))))
-	http.HandleFunc("DELETE /objects/{id}/projects/{projectId}", limiterMedium.RateLimit(containerBackoffice(auth_middleware.IsAuth(object_handlers.UnlinkProjectHandler))))
+	http.HandleFunc("POST /objects/{id}/projects/{projectId}", limiterMedium.RateLimit(auth_middleware.IsAuth(object_handlers.LinkProjectHandler)))
+	http.HandleFunc("DELETE /objects/{id}/projects/{projectId}", limiterMedium.RateLimit(auth_middleware.IsAuth(object_handlers.UnlinkProjectHandler)))
 	http.HandleFunc("GET /objects/{id}/users", limiterHigh.RateLimit(object_handlers.GetObjectUsersHandler))
-	http.HandleFunc("POST /objects/{id}/users/{userId}", limiterMedium.RateLimit(containerBackoffice(auth_middleware.IsAuth(object_handlers.LinkUserHandler))))
-	http.HandleFunc("DELETE /objects/{id}/users/{userId}", limiterMedium.RateLimit(containerBackoffice(auth_middleware.IsAuth(object_handlers.UnlinkUserHandler))))
+	http.HandleFunc("POST /objects/{id}/users/{userId}", limiterMedium.RateLimit(auth_middleware.IsAuth(object_handlers.LinkUserHandler)))
+	http.HandleFunc("DELETE /objects/{id}/users/{userId}", limiterMedium.RateLimit(auth_middleware.IsAuth(object_handlers.UnlinkUserHandler)))
 
 	// Project routes
 	http.HandleFunc("GET /projects", limiterHigh.RateLimit(project_handlers.GetProjectsHandler))
 	http.HandleFunc("GET /projects/{id}", limiterHigh.RateLimit(project_handlers.GetProjectHandler))
-	http.HandleFunc("POST /projects", limiterMedium.RateLimit(containerBackoffice(auth_middleware.IsAuth(project_handlers.CreateProjectHandler))))
-	http.HandleFunc("PUT /projects/{id}", limiterMedium.RateLimit(containerBackoffice(auth_middleware.IsAuth(project_handlers.UpdateProjectHandler))))
-	http.HandleFunc("DELETE /projects/{id}", limiterMedium.RateLimit(containerBackoffice(auth_middleware.IsAuth(project_handlers.DeleteProjectHandler))))
+	http.HandleFunc("POST /projects", limiterMedium.RateLimit(auth_middleware.IsAuth(project_handlers.CreateProjectHandler)))
+	http.HandleFunc("PUT /projects/{id}", limiterMedium.RateLimit(auth_middleware.IsAuth(project_handlers.UpdateProjectHandler)))
+	http.HandleFunc("DELETE /projects/{id}", limiterMedium.RateLimit(auth_middleware.IsAuth(project_handlers.DeleteProjectHandler)))
 	http.HandleFunc("GET /projects/{id}/score", limiterHigh.RateLimit(project_handlers.GetProjectScoreHandler))
 	http.HandleFunc("GET /projects/{id}/objects", limiterHigh.RateLimit(project_handlers.GetProjectObjectsHandler))
-	http.HandleFunc("POST /projects/{id}/objects/{objectId}", limiterMedium.RateLimit(containerBackoffice(auth_middleware.IsAuth(project_handlers.LinkObjectHandler))))
+	http.HandleFunc("POST /projects/{id}/objects/{objectId}", limiterMedium.RateLimit(auth_middleware.IsAuth(project_handlers.LinkObjectHandler)))
 	http.HandleFunc("GET /projects/{id}/steps", limiterHigh.RateLimit(project_handlers.GetProjectStepsHandler))
-	http.HandleFunc("POST /projects/{id}/steps", limiterMedium.RateLimit(containerBackoffice(auth_middleware.IsAuth(project_handlers.CreateProjectStepHandler))))
+	http.HandleFunc("POST /projects/{id}/steps", limiterMedium.RateLimit(auth_middleware.IsAuth(project_handlers.CreateProjectStepHandler)))
 
 	// Step routes
 	http.HandleFunc("GET /steps", limiterHigh.RateLimit(step_handlers.GetStepsHandler))
 	http.HandleFunc("GET /steps/{id}", limiterHigh.RateLimit(step_handlers.GetStepHandler))
-	http.HandleFunc("POST /steps", limiterMedium.RateLimit(containerBackoffice(auth_middleware.IsAuth(step_handlers.CreateStepHandler))))
-	http.HandleFunc("PUT /steps/{id}", limiterMedium.RateLimit(containerBackoffice(auth_middleware.IsAuth(step_handlers.UpdateStepHandler))))
-	http.HandleFunc("DELETE /steps/{id}", limiterMedium.RateLimit(containerBackoffice(auth_middleware.IsAuth(step_handlers.DeleteStepHandler))))
+	http.HandleFunc("POST /steps", limiterMedium.RateLimit(auth_middleware.IsAuth(step_handlers.CreateStepHandler)))
+	http.HandleFunc("PUT /steps/{id}", limiterMedium.RateLimit(auth_middleware.IsAuth(step_handlers.UpdateStepHandler)))
+	http.HandleFunc("DELETE /steps/{id}", limiterMedium.RateLimit(auth_middleware.IsAuth(step_handlers.DeleteStepHandler)))
 
 	logger.Info().Msg("Listening at http://localhost:" + os.Getenv("APP_PORT"))
 	err := http.ListenAndServe(":"+os.Getenv("APP_PORT"), nil)
